558e8e7d82acc1acfd7702746ff11e5838c38950
Chris James Martin <cj@cjmart.in>
2014-03-12 20:22:55 -0700

    Initial Commit

---
+++ tomatosauce/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tomatosauce
+//
+//  Created by Chris Martin on 3/12/14.
+//  Copyright (c) 2014 Chris Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tomatosauceTests/tomatosauceTests.m
@@ -0,0 +1,34 @@
+//
+//  tomatosauceTests.m
+//  tomatosauceTests
+//
+//  Created by Chris Martin on 3/12/14.
+//  Copyright (c) 2014 Chris Martin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tomatosauceTests : XCTestCase
+
+@end
+
+@implementation tomatosauceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
